41e9c12b7cebd2ac4fe110d7d6da1c361f27617e
shiv <shiv@revinfotech.com>
2019-02-07 17:40:15 +0530

    animation started

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Layout from './Hoc/Layout'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './components/home'
 
 const Routes = (props) => {
   return (
     <Layout>
-      Second Line
+      <Switch>
+        <Route exact component={Home} path="/"></Route>
+      </Switch>
     </Layout>
   )
 }
