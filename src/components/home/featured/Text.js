8ad908b165ee5abb0d38bea5db564d45d96e6edb
shiv <shiv@revinfotech.com>
2019-02-08 17:15:23 +0530

    text animation started

---
+++ src/components/home/featured/Text.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { easePolyOut } from 'd3-ease'
+import Animate from 'react-move/Animate'
+
+export default class Text extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
